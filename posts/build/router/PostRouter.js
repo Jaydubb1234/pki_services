638b67ea2013c4a53b9419faf2f94376a20d422e
Njume <Njinimbam@Njumes-MacBook-Pro.local>
2018-09-03 15:11:12 -0700

    create posts

--- posts/build/router/PostRouter.js
+++ posts/build/router/PostRouter.js
@@ -13,51 +13,16 @@ var PostRouter = /** @class */ (function () {
         var _a = req.body, currentUserId = _a.currentUserId, lastId = _a.lastId;
         var sqlQuery;
         lastId ?
-            sqlQuery = "SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,\n            u.display_name \n          FROM wordpresstest.wp_posts as wp\n          INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author\n          WHERE post_status = 'publish'\n          AND post_type = 'post'\n          AND post_author \n          IN (SELECT leader_id \n          FROM wordpresstest.wp_bp_follow \n          WHERE follower_id = " + currentUserId + ")\n          AND wp.ID < " + lastId + "\n          ORDER BY post_date DESC\n          LIMIT 10"
+            sqlQuery = "SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,\n          u.display_name \n          FROM wordpresstest.wp_posts as wp\n          INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author\n          WHERE post_status = 'publish'\n          AND post_type = 'post'\n          AND post_author \n          IN (SELECT leader_id \n          FROM wordpresstest.wp_bp_follow \n          WHERE follower_id = " + currentUserId + ")\n          AND wp.ID < " + lastId + "\n          ORDER BY post_date DESC\n          LIMIT 10"
             :
-                sqlQuery = "SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,\n            u.display_name \n          FROM wordpresstest.wp_posts as wp\n          INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author\n          WHERE post_status = 'publish'\n          AND post_type = 'post'\n          AND post_author \n          IN (SELECT leader_id \n          FROM wordpresstest.wp_bp_follow \n          WHERE follower_id = " + currentUserId + ")\n          ORDER BY post_date DESC\n          LIMIT 10";
-        db_1.mysqlDb
-            .then(function (client) {
-            client.query(sqlQuery, function (err, results, fields) {
-                if (err)
-                    return res.status(500).json({ err: err });
-                return res.status(200).json({
-                    code: res.statusCode,
-                    message: "OK",
-                    datetime: new Date(),
-                    data: results
-                });
-                db_1.mysqlDb.close();
-            });
-        })
-            .catch(function (err) {
-            console.error(err);
-            console.error(res.status(500).json({ err: err }));
-            throw new Error(err);
-        });
+                sqlQuery = "SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,\n          u.display_name \n          FROM wordpresstest.wp_posts as wp\n          INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author\n          WHERE post_status = 'publish'\n          AND post_type = 'post'\n          AND post_author \n          IN (SELECT leader_id \n          FROM wordpresstest.wp_bp_follow \n          WHERE follower_id = " + currentUserId + ")\n          ORDER BY post_date DESC\n          LIMIT 10";
+        queryToMysql(res, sqlQuery);
     };
     //get single post by author
     PostRouter.prototype.getPost = function (req, res) {
         var postAuthorId = req.params.postAuthorId;
         var sqlQuery = "SELECT * FROM wordpresstest.wp_posts WHERE post_author = " + postAuthorId;
-        db_1.mysqlDb
-            .then(function (client) {
-            client.query(sqlQuery, function (err, results, fields) {
-                if (err)
-                    return res.status(500).json({ err: err });
-                return res.status(200).json({
-                    code: res.statusCode,
-                    message: "OK",
-                    datetime: new Date(),
-                    data: results
-                });
-                db_1.mysqlDb.close();
-            });
-        })
-            .catch(function (err) {
-            console.error(res.status(500).json({ err: err }));
-            throw new Error(err);
-        });
+        queryToMysql(res, sqlQuery);
     };
     // get all of the posts in the database
     // public all(req: Request, res: Response): void {
@@ -80,33 +45,13 @@ var PostRouter = /** @class */ (function () {
     //       res.status(500).json({ error });
     //     });
     // }
-    // // create a new post
-    // public create(req: Request, res: Response): void {
-    //   const {
-    //     title,
-    //     slug,
-    //     content,
-    //     featuredImage,
-    //     category,
-    //     published,
-    //   } = req.body;
-    //   const post = new Post({
-    //     title,
-    //     slug,
-    //     content,
-    //     featuredImage,
-    //     category,
-    //     published,
-    //   });
-    //   post
-    //     .save()
-    //     .then((data) => {
-    //       res.status(201).json({ data });
-    //     })
-    //     .catch((error) => {
-    //       res.status(500).json({ error });
-    //     });
-    // }
+    // create a new post
+    PostRouter.prototype.createPost = function (req, res) {
+        var _a = req.body.postContent, body = _a.body, creationDate = _a.creationDate;
+        var currentUserId = req.body.currentUserId;
+        var sqlQuery = "INSERT INTO wordpresstest.wp_posts\n      (\n        post_author,\n        post_date,\n        post_date_gmt,\n        post_content,\n        post_title,\n        post_excerpt,\n        post_status,\n        comment_status,\n        ping_status,\n        post_password,\n        post_name,\n        to_ping,\n        pinged,\n        post_modified,\n        post_modified_gmt,\n        post_content_filtered,\n        post_parent,\n        guid,\n        menu_order,\n        post_type,\n        post_mime_type,\n        comment_count\n      )\n    VALUES\n      (\n        " + currentUserId + ",\n        '" + creationDate + "',\n        '" + creationDate + "',\n        '" + body + "',\n        'post_title',\n        '',\n        'publish',\n        'open',\n        'open',\n        '',\n        'post_name',\n        '',\n        '',\n        '" + creationDate + "',\n        '" + creationDate + "',\n        '',\n        '0',\n        'guid',\n        'menu_order',\n        'post',\n        '',\n        '0'\n      )";
+        queryToMysql(res, sqlQuery);
+    };
     // // update post by params of 'slug'
     // public update(req: Request, res: Response): void {
     //   const { slug } = req.body;
@@ -129,16 +74,60 @@ var PostRouter = /** @class */ (function () {
     //       res.status(500).json({ error });
     //     });
     // }
+    // public function queryToMysql(res: Response, sqlQuery: String) {
+    //   mysqlDb
+    //     .then( (client) => {
+    //       client.query(sqlQuery, 
+    //       (err, results, fields) =>{
+    //         if (err) return res.status(500).json({ err })
+    //         return res.status(200).json(
+    //           {
+    //             code: res.statusCode,
+    //             message: "OK",
+    //             datetime: new Date(),
+    //             data:results
+    //           }
+    //         );
+    //         mysqlDb.close()
+    //       })
+    //     })
+    //     .catch( (err) => {
+    //       console.error(err)
+    //       console.error(res.status(500).json({ err }))
+    //       throw new Error(err)
+    //     })
+    // }
     PostRouter.prototype.routes = function () {
         this.router.post('/', this.getPosts);
         this.router.get('/:currentUserId', this.getPost);
-        // this.router.post('/', this.create);
+        this.router.post('/create', this.createPost);
         // this.router.put('/:slug', this.update);
         // this.router.delete('/:slug', this.delete);
     };
     return PostRouter;
 }());
 exports.PostRouter = PostRouter;
+function queryToMysql(res, sqlQuery) {
+    db_1.mysqlDb
+        .then(function (client) {
+        client.query(sqlQuery, function (err, results, fields) {
+            if (err)
+                return res.status(500).json({ err: err });
+            return res.status(200).json({
+                code: res.statusCode,
+                message: "OK",
+                datetime: new Date(),
+                data: results
+            });
+            db_1.mysqlDb.close();
+        });
+    })
+        .catch(function (err) {
+        console.error(err);
+        console.error(res.status(500).json({ err: err }));
+        throw new Error(err);
+    });
+}
 var postRoutes = new PostRouter();
 postRoutes.routes();
 exports.default = postRoutes.router;
--- posts/src/router/PostRouter.ts
+++ posts/src/router/PostRouter.ts
@@ -13,10 +13,10 @@ export class PostRouter {
   //get all current user post and followers post
   public getPosts(req: Request, res: Response): void {
     const { currentUserId, lastId } = req.body;
-      let sqlQuery
-        lastId ? 
-          sqlQuery = `SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,
-            u.display_name 
+    let sqlQuery
+      lastId ? 
+        sqlQuery = `SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,
+          u.display_name 
           FROM wordpresstest.wp_posts as wp
           INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author
           WHERE post_status = 'publish'
@@ -28,9 +28,9 @@ export class PostRouter {
           AND wp.ID < ${lastId}
           ORDER BY post_date DESC
           LIMIT 10` 
-        :
-          sqlQuery = `SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,
-            u.display_name 
+      :
+        sqlQuery = `SELECT wp.post_content, wp.post_title, wp.ID as id, wp.comment_count, wp.post_date, wp.post_author, wp.post_modified,
+          u.display_name 
           FROM wordpresstest.wp_posts as wp
           INNER JOIN wordpresstest.wp_users as u ON u.ID = wp.post_author
           WHERE post_status = 'publish'
@@ -42,54 +42,14 @@ export class PostRouter {
           ORDER BY post_date DESC
           LIMIT 10`
 
-
-      mysqlDb
-        .then( (client) => {
-            client.query(sqlQuery, 
-            (err, results, fields) =>{
-                if (err) return res.status(500).json({ err })
-                return res.status(200).json(
-                  {
-                    code: res.statusCode,
-                    message: "OK",
-                    datetime: new Date(),
-                    data:results
-                  }
-                );
-                mysqlDb.close()
-            })
-        })
-        .catch( (err) => {
-            console.error(err)
-            console.error(res.status(500).json({ err }))
-            throw new Error(err)
-        })
+    queryToMysql(res,sqlQuery)
   }
 
   //get single post by author
   public getPost(req: Request, res: Response): void {
-    const { postAuthorId } = req.params;
-      const sqlQuery = `SELECT * FROM wordpresstest.wp_posts WHERE post_author = ${postAuthorId}`
-      mysqlDb
-        .then( (client) => {
-            client.query(sqlQuery, 
-            (err, results, fields) =>{
-                if (err) return res.status(500).json({ err })
-                return res.status(200).json(
-                  {
-                    code: res.statusCode,
-                    message: "OK",
-                    datetime: new Date(),
-                    data:results
-                  }
-                );
-                mysqlDb.close()
-            })
-        })
-        .catch( (err) => {
-            console.error(res.status(500).json({ err }))
-            throw new Error(err)
-        })
+    const { postAuthorId } = req.params
+    const sqlQuery = `SELECT * FROM wordpresstest.wp_posts WHERE post_author = ${postAuthorId}`
+    queryToMysql(res,sqlQuery)
   }
 
   // get all of the posts in the database
@@ -116,35 +76,64 @@ export class PostRouter {
   //     });
   // }
 
-  // // create a new post
-  // public create(req: Request, res: Response): void {
-  //   const {
-  //     title,
-  //     slug,
-  //     content,
-  //     featuredImage,
-  //     category,
-  //     published,
-  //   } = req.body;
+  // create a new post
+  public createPost(req: Request, res: Response): void {
+    const { body, creationDate } = req.body.postContent;
+    const { currentUserId } = req.body
 
-  //   const post = new Post({
-  //     title,
-  //     slug,
-  //     content,
-  //     featuredImage,
-  //     category,
-  //     published,
-  //   });
-
-  //   post
-  //     .save()
-  //     .then((data) => {
-  //       res.status(201).json({ data });
-  //     })
-  //     .catch((error) => {
-  //       res.status(500).json({ error });
-  //     });
-  // }
+    const sqlQuery = `INSERT INTO wordpresstest.wp_posts
+      (
+        post_author,
+        post_date,
+        post_date_gmt,
+        post_content,
+        post_title,
+        post_excerpt,
+        post_status,
+        comment_status,
+        ping_status,
+        post_password,
+        post_name,
+        to_ping,
+        pinged,
+        post_modified,
+        post_modified_gmt,
+        post_content_filtered,
+        post_parent,
+        guid,
+        menu_order,
+        post_type,
+        post_mime_type,
+        comment_count
+      )
+    VALUES
+      (
+        ${currentUserId},
+        '${creationDate}',
+        '${creationDate}',
+        '${body}',
+        'post_title',
+        '',
+        'publish',
+        'open',
+        'open',
+        '',
+        'post_name',
+        '',
+        '',
+        '${creationDate}',
+        '${creationDate}',
+        '',
+        '0',
+        'guid',
+        'menu_order',
+        'post',
+        '',
+        '0'
+      )`
+    
+    queryToMysql(res,sqlQuery)
+  }
 
   // // update post by params of 'slug'
   // public update(req: Request, res: Response): void {
@@ -172,13 +161,61 @@ export class PostRouter {
   //     });
   // }
 
+  // public function queryToMysql(res: Response, sqlQuery: String) {
+  //   mysqlDb
+  //     .then( (client) => {
+  //       client.query(sqlQuery, 
+  //       (err, results, fields) =>{
+  //         if (err) return res.status(500).json({ err })
+  //         return res.status(200).json(
+  //           {
+  //             code: res.statusCode,
+  //             message: "OK",
+  //             datetime: new Date(),
+  //             data:results
+  //           }
+  //         );
+  //         mysqlDb.close()
+  //       })
+  //     })
+  //     .catch( (err) => {
+  //       console.error(err)
+  //       console.error(res.status(500).json({ err }))
+  //       throw new Error(err)
+  //     })
+  // }
+
   public routes() {
     this.router.post('/', this.getPosts);
     this.router.get('/:currentUserId', this.getPost);
-    // this.router.post('/', this.create);
+    this.router.post('/create', this.createPost);
     // this.router.put('/:slug', this.update);
     // this.router.delete('/:slug', this.delete);
   }
+}
+
+function queryToMysql(res: Response, sqlQuery: String): void {
+  mysqlDb
+    .then( (client) => {
+      client.query(sqlQuery, 
+      (err, results, fields) =>{
+        if (err) return res.status(500).json({ err })
+        return res.status(200).json(
+          {
+            code: res.statusCode,
+            message: "OK",
+            datetime: new Date(),
+            data:results
+          }
+        );
+        mysqlDb.close()
+      })
+    })
+    .catch( (err) => {
+      console.error(err)
+      console.error(res.status(500).json({ err }))
+      throw new Error(err)
+    })
 }
 
 const postRoutes = new PostRouter();
